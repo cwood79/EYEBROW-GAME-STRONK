ecbc76feb70213e702ed0cb4e4ebf75b6236e337
cwood79 <chris15618@gmail.com>
2014-11-15 14:42:10 -0800

    basic moving for detective, stuff can be picked up

---
+++ New Unity Project 1/Assets/Detective.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detective : MonoBehaviour {
+	public float jumpForce = 15f;
+	public float jumpTime = 0.25f;
+	public float moveSpeed = 10f;
+	
+	public float addPoints = 20;
+	
+	public static float points = 0;
+	
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKey (KeyCode.D)) {
+			this.transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+		}
+		if(Input.GetKey (KeyCode.A)) {
+			this.transform.Translate(new Vector3(-moveSpeed, 0, 0)* Time.deltaTime);
+		}
+		
+		// Jump
+		if(Input.GetKey (KeyCode.W)) {
+			this.rigidbody2D.AddForce(Vector3.up * jumpForce);
+		}
+	}
+	
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.name == "collectable" && Input.GetKey (KeyCode.E)) {
+			points += addPoints;
+			Destroy (collision.gameObject);
+			print("Current points " + points);
+		}
+	}
+}
--- New Unity Project 1/Assets/Thief.cs
+++ New Unity Project 1/Assets/Thief.cs
@@ -8,10 +8,10 @@ public class Thief : MonoBehaviour {
 	public float moveSpeed = 20f;
 	
 	public float addPoints = 20;
-
+	
 	public static float points = 0;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		//thief = GameObject.find("kuckles_tails_thief");
@@ -19,36 +19,36 @@ public class Thief : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if(Input.GetKey(KeyCode.RightArrow)) {
+		
+		if(Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
-					}
+		}
 		if(Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate(new Vector3(-moveSpeed, 0, 0)* Time.deltaTime);
 		}
-
+		
 		// Jump
 		if(Input.GetKey (KeyCode.UpArrow)) {
 			rigidbody2D.AddForce(Vector3.up * jumpForce);
-			}
+		}
 	}
-		             
-
+	
+	
 	void OnCollisionEnter2D(Collision2D collision) {
-			if (collision.gameObject.name == "collectable"){// && Input.GetKey (KeyCode.RightShift)) {
-				points += addPoints;
-				Destroy (collision.gameObject);
-				print("Current points " + points);
-				}
-			//if (collision.gameObject.name == "bottom" && Input.GetKey (KeyCode.UpArrow)) {
-			//	rigidbody2D.AddForce(Vector3.up * jumpForce);
-			//	} 
-			// unlock door
+		if (collision.gameObject.name == "collectable" && Input.GetKey (KeyCode.RightShift)) {
+			points += addPoints;
+			Destroy (collision.gameObject);
+			print("Current points " + points);
+		}
+		//if (collision.gameObject.name == "bottom" && Input.GetKey (KeyCode.UpArrow)) {
+		//	rigidbody2D.AddForce(Vector3.up * jumpForce);
+		//	} 
+		// unlock door
 		//		if (Collision.GameObject.namespace == "door" &&  Input.getKey("RightShift"))// use action key) {
-
-	//	}
-			// climb ladder
-			//if (collision.gameObject.namespace == "ladder" && Input.getKey("Up") {
-	//	}
+		
+		//	}
+		// climb ladder
+		//if (collision.gameObject.namespace == "ladder" && Input.getKey("Up") {
+		//	}
 	}
 }
