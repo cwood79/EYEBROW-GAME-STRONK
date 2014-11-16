dcff4e17033017a09ddbd0bb76c2c534e226c98a
cwood79 <chris15618@gmail.com>
2014-11-15 17:02:09 -0800

    THIEF FIXED FOR REAL

--- New Unity Project 1/Assets/Scripts/Thief.cs
+++ New Unity Project 1/Assets/Scripts/Thief.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class Thief : MonoBehaviour {
 	//private GameObject thief;
-	public float jumpForce = 30f;
-	public float jumpTime = 0.5f;
+	public float jumpForce = 550f;
 	public float moveSpeed = 20f;
 	
 	public float addPoints = 20;
 	
 	public static float points = 0;
+
+	public bool grounded = true;
 	
 	
 	// Use this for initialization
@@ -26,15 +27,29 @@ public class Thief : MonoBehaviour {
 		if(Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate(new Vector3(-moveSpeed, 0, 0)* Time.deltaTime);
 		}
-		
+
+	}
+
+	void FixedUpdate() {
+		print("Grounded is " + grounded);
+		print ("Velocity is " + rigidbody2D.velocity.y);
 		// Jump
-		if(Input.GetKey (KeyCode.UpArrow)) {
-			rigidbody2D.AddForce(Vector3.up * jumpForce);
+		if(grounded && Input.GetKey (KeyCode.UpArrow)) {
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
 		}
+		if (rigidbody2D.velocity.y >= 6) {
+			grounded = false;
+		}
+		
 	}
 	
 	
 	void OnCollisionStay2D(Collision2D collision) {
+
+		if (collision.gameObject.name == "ground") {
+			grounded = true;
+		}
+
 		if (collision.gameObject.name == "collectable" && Input.GetKey (KeyCode.RightShift)) {
 			points += addPoints;
 			Destroy (collision.gameObject);
