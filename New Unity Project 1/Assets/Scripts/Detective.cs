a7cd0b3305b3fd6bad7f639839197a2b77bfe63a
quesadilla <j4jimene@ucsd.edu>
2014-11-15 17:39:36 -0800

    Detective can pass through Thief
    also changed size of collectibles

--- New Unity Project 1/Assets/Scripts/Detective.cs
+++ New Unity Project 1/Assets/Scripts/Detective.cs
@@ -2,25 +2,34 @@
 using System.Collections;
 
 public class Detective : MonoBehaviour {
-	public float jumpForce = 100f;
-	public float moveSpeed = 10f;
-	
-	public float addPoints = 20;
+	public float jumpForce = 100f; // vertical speed
+	public float moveSpeed = 10f; // horizontal speed
+	public const float ADD_POINTS = 20;
 	
 	public static float points = 0;
+	public const int MAX_V = 2;
 
 	public bool grounded = true;
-	
+
+	public GameObject thief;
+
 	// Use this for initialization
 	void Start () {
+
+		// allow detectie to pass through thief
+		thief = GameObject.Find ("kuckles_tails_thief");
+		Physics2D.IgnoreCollision( thief.collider2D, this.collider2D);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// move right
 		if(Input.GetKey (KeyCode.D)) {
 			this.transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
 		}
+
+		// move left
 		if(Input.GetKey (KeyCode.A)) {
 			this.transform.Translate(new Vector3(-moveSpeed, 0, 0)* Time.deltaTime);
 		}
@@ -28,30 +37,28 @@ public class Detective : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		print("Grounded is " + grounded);
-		print ("Velocity is " + rigidbody2D.velocity.y);
 		// Jump
 		if(grounded && Input.GetKey (KeyCode.W)) {
 			rigidbody2D.AddForce(new Vector2(0,jumpForce));
 		}
-		if (rigidbody2D.velocity.y >= 2) {
+		// controls length of jump
+		if (rigidbody2D.velocity.y >= MAX_V) {
 			grounded = false;
-				}
+		}
 
 	}
 
-	//void OnCollisionEnter2D(Collision2D collision){
-
-	//	}
-
 
 	void OnCollisionStay2D(Collision2D collision) {
+
+		// allow player to jump
 		if (collision.gameObject.name == "ground") {
 						grounded = true;
-				}
+		}
 
+		// collect items
 		if (collision.gameObject.name == "collectable" && Input.GetKey (KeyCode.E)) {
-			points += addPoints;
+			points += ADD_POINTS;
 			Destroy (collision.gameObject);
 			print("Current points " + points);
 		}
